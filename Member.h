1b14d13be646587132a25aaf270e6f4d40133b63
Daniel Binyamin <danielbinyamin@gmail.com>
2018-03-27 18:24:54 +0300

    fixed numUsers bug

--- Member.h
+++ Member.h
@@ -2,25 +2,30 @@
 #include <array>
 #include <string>
 #include <iostream>
+
 #pragma once
 
-static int numUsers,idCreator;
+static int numUsers, idCreator;
 
 using namespace std;
 
 class Member {
 
-    private:
-        vector<Member> followList; //list of people i follow
-        vector<Member> followMeList; //list of people who follow me
-        int id;
+private:
+    vector<Member*> followList; //list of people i follow
+    vector<Member*> followMeList; //list of people who follow me
+    int id;
 
 public:
 
     void follow(Member &other);
+
     void unfollow(Member &other);
+
     int numFollowers();
+
     int numFollowing();
+
     static int count() { return numUsers; }
 
     //constructor
@@ -28,8 +33,8 @@ public:
         id = idCreator; //create specific id for Member
         ++idCreator;
         ++numUsers;
-        cout<<"number of users: "<<numUsers<<endl;
-        cout<<"idCreator: "<<idCreator<<endl;
+//        cout << "number of users: " << numUsers << endl;
+//        cout << "idCreator: " << idCreator << endl;
     }
 
     //destructor
